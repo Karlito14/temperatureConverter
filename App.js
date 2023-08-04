d630d865771897c385bdff41ba91ecc6840b595a
Karlito14 <leiroz26@hotmail.com>
2023-08-04 11:30:34 +0200

    layout

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
+import { style } from "./App.style";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <View style={style.workspace}>
+        <View><Text>Température</Text></View>
+        <View><Text>Input</Text></View>
+        <View><Text>Button</Text></View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ App.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: 'orange',
+        justifyContent: 'center'
+    },
+    workspace : {
+        height: 450,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    }
+});
